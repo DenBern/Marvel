d711defff4754bb944982d91e4c3d5b94e5ca977
Bernovich Denis <denisbernovich@gmail.com>
2023-02-17 20:33:22 +0300

    react_152 done

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -80,10 +80,17 @@ class CharInfo extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+
+    let imgStyle = {'objectFit' : 'cover'};
+
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
     return (
         <>
             <div className="char__basics">
-                    <img src={thumbnail} alt="abyss"/>
+                    <img src={thumbnail} alt={name} style={imgStyle}/>
                     <div>
                         <div className="char__info-name">{name}</div>
                         <div className="char__btns">
@@ -101,8 +108,11 @@ const View = ({char}) => {
                 </div>
                 <div className="char__comics">Comics:</div>
                 <ul className="char__comics-list">
+                    {comics.length > 0 ? null : "There is no comics with this character"}
                     {
                         comics.map((item, index) => {
+                            // eslint-disable-next-line 
+                            if (index > 9) return;
                             return (
                                 <li key={index} className="char__comics-item">
                                     {item.name}
