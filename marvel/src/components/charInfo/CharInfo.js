99f9d6ff0ebee0186549fd175d4f1616133b0467
Denis Bernovich <denisbernovich@gmail.com>
2023-02-20 19:42:27 +0300

    react_155

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import { Spinner } from '../spinner/Spinner';
 import { ErrorMessage } from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
@@ -125,4 +127,7 @@ const View = ({char}) => {
     )
 }
 
+CharInfo.propTypes = {
+    charId: PropTypes.number,
+}
 export default CharInfo;
