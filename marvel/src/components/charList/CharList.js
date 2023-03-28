a2007ed92abaeaf75ef569d5ac8da4dbb27fb122
Denis Bernovich <denisbernovich@gmail.com>
2023-03-28 11:05:49 +0300

    173 react router

--- marvel/src/components/charList/CharList.js
+++ marvel/src/components/charList/CharList.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-
 import useMarvelService from '../../services/MarvelService';
 import { Spinner } from '../spinner/Spinner';
 import { ErrorMessage } from '../errorMessage/ErrorMessage';
